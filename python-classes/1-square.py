a6e916376ec7efcc6bc00551e3c38cae95ecefac
tamerlanhtm <tamerlan_htm@hotmail.com>
2025-05-19 05:09:53 +0000

    1-square.py

---
+++ python-classes/1-square.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""This module defines a class named Square."""
+
+
+class Square:
+    """This class defines a square with a specified size."""
+    def __init__(self, size):
+        self.__size = size
