31779161d02af4f307f5c573b0b36197cb904d81
root <tamerlan_htm@hotmail.com>
2025-05-26 14:12:24 +0000

    8-rectangle.py

---
+++ python-inheritance/8-rectangle.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""This class contains class named BaseGeometry"""
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """
+    Rectangle class.
+
+    Attributes
+    ----------
+        __width : int
+        __height : int
+    """
+    def __init__(self, width, height):
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
