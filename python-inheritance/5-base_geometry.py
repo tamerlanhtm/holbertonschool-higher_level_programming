88fc43e3da45a75a450b8f65f87f5fffde0b5959
root <tamerlan_htm@hotmail.com>
2025-05-26 11:45:42 +0000

    56

---
+++ python-inheritance/5-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""This class contains empty class named BaseGeometry"""
+
+
+class BaseGeometry:
+    """
+    Empty class
+    """
+    pass
---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""This class contains class named BaseGeometry"""
+
+
+class BaseGeometry:
+    """
+       BaseGeometry class
+
+       Method
+       ------
+            area():
+    """
+    def area(self):
+        """
+        Area method
+
+        Raise
+        -----
+            Exception:
+                area() is not implemented
+        """
+        raise Exception("area() is not implemented")
