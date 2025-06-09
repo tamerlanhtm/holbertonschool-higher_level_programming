3bf1550f676e20782c2784a115ec5856189abc62
tamerlanhtm <tamerlan_htm@hotmail.com>
2025-06-09 09:20:43 +0000

    task_02_requests.py

---
+++ restful-api/task_02_requests.py
@@ -0,0 +1,26 @@
+import requests
+import csv
+
+def fetch_and_print_posts():
+    response = requests.get("https://jsonplaceholder.typicode.com/posts")
+    print(f"Status Code: {response.status_code}")
+
+    if response.status_code == 200:
+        posts = response.json()
+        for post in posts:
+            print(post['title'])
+
+def fetch_and_save_posts():
+    response = requests.get("https://jsonplaceholder.typicode.com/posts")
+
+    if response.status_code == 200:
+        posts = response.json()
+
+        posts_data = [{"id": post['id'], "title": post['title'], "body": post['body']} for post in posts]
+
+        with open('posts.csv', mode='w', newline='', encoding='utf-8') as file:
+            writer = csv.DictWriter(file, fieldnames=["id", "title", "body"])
+            writer.writeheader()
+            writer.writerows(posts_data)
+
+        print("Posts have been saved to posts.csv.")
